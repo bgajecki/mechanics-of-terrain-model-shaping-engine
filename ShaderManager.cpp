772f551ad4b4c890c5860fedd9bc42ba72f97fa0
Beniamin Gajecki <beniamin.gajecki@gmail.com>
2022-08-19 12:05:13 +0200

    Include shader classes and programs to the ShaderManager class that manages them.

---
+++ ShaderManager.cpp
@@ -0,0 +1,34 @@
+﻿#include "ShaderManager.hpp"
+
+namespace Engine
+{
+	ShaderManager::~ShaderManager()
+	{
+		this->deletePrograms();
+	}
+
+	Program* ShaderManager::createProgram()
+	{
+		this->programs.push_back(std::make_unique<Program>());
+		auto programPointer = this->programs.back().get();
+		return programPointer;
+	}
+
+	void ShaderManager::deleteProgram(Program* programPointer)
+	{
+		
+		for(auto program = this->programs.begin(); program != this->programs.end();)
+			if(programPointer == program->get())
+				this->programs.erase(program);
+		/*
+		for (ProgramsVector::iterator program : this->programs)
+			if (programPointer == program->get())
+				this->programs.erase(program);
+		*/
+	}
+
+	void ShaderManager::deletePrograms()
+	{
+		this->programs.clear();
+	}
+}
---
+++ ShaderManager.hpp
@@ -0,0 +1,66 @@
+﻿#pragma once
+
+
+#include <vector>
+#include "Program.hpp"
+
+
+namespace Engine
+{
+	/**
+	* @brief Unique pointer to program object.
+	*/
+	typedef std::unique_ptr<Program> ProgramPointer;
+
+	/**
+	* @brief Vector of unique pointers to program objects.
+	*/
+	typedef std::vector<ProgramPointer> ProgramsVector;
+
+	/**
+	* @brief A shader manager class that manages shaders in the stage.
+	*/
+	class ShaderManager
+	{
+	public:
+
+		ShaderManager() = default;
+
+		/**
+		* Delete all programs.
+		*/
+		~ShaderManager();
+
+		/**
+		* @brief Only one istance of ShaderManager object.
+		*/
+		ShaderManager(const ShaderManager&) = delete;
+		/**
+		* @brief Only one istance of ShaderManager object.
+		*/
+		ShaderManager(ShaderManager&&) = delete;
+
+		/**
+		* @brief Create program and return instance.
+		* @return Pointer to program.
+		*/
+		Program* createProgram();
+
+		/**
+		* @brief Delete program.
+		* @param program Pointer to program.
+		*/
+		void deleteProgram(Program*);
+
+		/**
+		* @brief Delete all programs.
+		*/
+		void deletePrograms();
+		
+	private:
+		/**
+		* @brief Vector of programs. Contains all programs used by stage.
+		*/	
+		ProgramsVector programs;
+	};
+}
